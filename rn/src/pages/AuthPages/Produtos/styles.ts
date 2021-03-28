b30c8243ab1a218ed2f04a5cf381f742c63efc39
Delfio Francisco <delfio_eu@hotmail.com>
2021-03-28 00:52:52 -0400

    listagem de produtos

--- rn/src/pages/AuthPages/Produtos/styles.ts
+++ rn/src/pages/AuthPages/Produtos/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
     flex: 1;
-    align-items: center;
     background-color: white;
 `;
 
@@ -57,18 +56,37 @@ export const InputPesquisaPedidos = styled.TextInput`
 
 export const WrapperProduct = styled.View`
     width: 100%;
-    background-color: red;
-    height: 90px;
-    padding: 5px 20px 5px;
+    /* background-color: red; */
+    height: 20%;
+    min-height: 100px;
+    padding: 5px 20px 10px;
     flex-direction: row;
+    border-bottom-width: 1px;
+    border-color: rgba(0, 0, 0, 0.3);
 `;
 
 export const WrapperProductImage = styled.View`
-    width: 30%;
-    background-color: blue;
+    width: 35%;
 `;
 
 export const WrapperProductInfo = styled.View`
     flex: 1;
-    background-color: green;
+    /* background-color: green; */
+    padding-left: 10px;
+    justify-content: center;
+`;
+
+export const ProductName = styled.Text`
+    font-size: 18px;
+    font-weight: 700;
+`;
+
+export const ProductDetails = styled.Text`
+    font-size: 15px;
+    font-weight: 400;
+`;
+
+export const ProductPrice = styled.Text`
+    font-size: 14px;
+    font-weight: 700;
 `;
